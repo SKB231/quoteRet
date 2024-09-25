cee9d121556013808a90c514a730f5068940bfa1
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2024-09-25 16:48:14 -0400

    Changed chart stuff

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,8 +20,6 @@ import (
 
 	"github.com/SKB231/quoteRet/linkParser"
 )
-
-
 
 type FakeUserAgentsResponse struct {
 	Result []string `json:"result"`
@@ -48,11 +48,11 @@ func GetUserAgentList(fakeUserAgentsBuffer chan string) {
 	}
 }
 
-func getQuotePriceAndTime(quote string,userAgent string, version int) (string, string) {
+func getQuotePriceAndTime(quote string, userAgent string, version int) (string, string) {
 	URL := fmt.Sprintf("https://finance.yahoo.com/quote/%v", quote)
-	reqUrl, err := url.Parse(URL);
+	reqUrl, err := url.Parse(URL)
 	if err != nil {
-		fmt.Println("Error with URL: ", err);
+		fmt.Println("Error with URL: ", err)
 	}
 	req := http.Request{
 		Method: "GET",
@@ -61,30 +61,28 @@ func getQuotePriceAndTime(quote string,userAgent string, version int) (string, s
 		},
 		URL: reqUrl,
 	}
-	resp, err := http.DefaultClient.Do(&req);
+	resp, err := http.DefaultClient.Do(&req)
 	if err != nil {
-		fmt.Println("Link Parser error: ", err);
-		return "", "";
+		fmt.Println("Link Parser error: ", err)
+		return "", ""
 	}
-	res, err := linkParser.Parse(resp.Body, quote);
+	res, err := linkParser.Parse(resp.Body, quote)
 
 	if err != nil {
-		fmt.Println("Link Parser error: ", err);
-		return "", "";
+		fmt.Println("Link Parser error: ", err)
+		return "", ""
 	}
 
 	return res.Price, res.Time
 }
 
-
 func getQuotePrice(quote string, userAgent string) (string, string) {
 	//strings.ReplaceAll(quote, ".", "/.")
-	resPrice, time := getQuotePriceAndTime(quote, userAgent, -1);
+	resPrice, time := getQuotePriceAndTime(quote, userAgent, -1)
 	return resPrice, time
 }
 
 var fakeUserAgents = make(chan string, 100)
-
 
 func handleStringQuotePrice(w http.ResponseWriter, r *http.Request) {
 	quoteName := r.URL.Query().Get("quote")
@@ -301,39 +299,81 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 type interval string
 
 const (
-	WEEK_INTEVAL interval = "1wk"
+	WEEK_INTEVAL   interval = "1wk"
 	MONTH_INTERVAL interval = "1mo"
-	DAY_INTERVAL interval = "1d"
+	DAY_INTERVAL   interval = "1d"
 )
 
-func getCSVData(quote string, startTime string, endTime string, hopTime interval, userAgent string) (string, error){
-	reqUrl := fmt.Sprintf("https://query1.finance.yahoo.com/v7/finance/download/%v?period1=%v&period2=%v&interval=%v", quote, startTime, endTime, hopTime)
+// func getCSVData(quote string, startTime string, endTime string, limit int, userAgent string) (string, error) {
+// 	reqUrl := fmt.Sprintf("https://query1.finance.yahoo.com/v7/finance/download/%v?period1=%v&period2=%v&interval=%v", quote, startTime, endTime, hopTime)
+// 	urlObj, err := url.Parse(reqUrl)
+
+// 	if err != nil {
+// 		fmt.Printf("Error occured when retriving historical data %v \n", err)
+// 		return "", err
+// 	}
+
+// 	req := http.Request{
+// 		Method: "GET",
+// 		Header: map[string][]string{
+// 			"User-Agent": []string{userAgent},
+// 		},
+// 		URL: urlObj,
+// 	}
+
+// 	resp, err := http.DefaultClient.Do(&req)
+// 	if err != nil {
+// 		fmt.Printf("Error occured when retriving historical data %v \n", err)
+// 		return "", err
+// 	}
+// 	data, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Printf("Error occured when retriving historical data %v \n", err)
+// 		return "", err
+// 	}
+
+// 	return string(data), nil
+// }
+
+func getCSVData(quote string, startDate, endDate, limit string, userAgent string) (string, error) {
+	// Generate a random number between 1 and 30
+	randomNum := rand.Intn(30) + 1
+
+	// Format the URL to match the Nasdaq API structure
+	reqUrl := fmt.Sprintf("https://api.nasdaq.com/api/quote/%v/historical?assetclass=stocks&fromdate=%v&todate=%v&limit=%v&random=%v",
+		quote, startDate, endDate, limit, randomNum)
+
 	urlObj, err := url.Parse(reqUrl)
-	
 	if err != nil {
-		fmt.Printf("Error occured when retriving historical data %v \n", err)
+		fmt.Printf("Error occurred when retrieving historical data: %v \n", err)
 		return "", err
 	}
 
+	// Build the HTTP request
 	req := http.Request{
 		Method: "GET",
 		Header: map[string][]string{
-			"User-Agent": []string{userAgent},
+			"User-Agent": {userAgent},
 		},
 		URL: urlObj,
 	}
 
-	resp, err := http.DefaultClient.Do(&req);
+	// Execute the HTTP request
+	resp, err := http.DefaultClient.Do(&req)
 	if err != nil {
-		fmt.Printf("Error occured when retriving historical data %v \n", err)
+		fmt.Printf("Error occurred when retrieving historical data: %v \n", err)
 		return "", err
 	}
+	defer resp.Body.Close()
+
+	// Read the response body
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error occured when retriving historical data %v \n", err)
+		fmt.Printf("Error occurred when retrieving historical data: %v \n", err)
 		return "", err
 	}
 
+	// Return the data as a string
 	return string(data), nil
 }
 
@@ -351,38 +391,120 @@ func getHistoricalData(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	unixTime := time.Unix(pi, 0)
-	var dayCount int64 = ((pi/3600.0)/24.0)
+	var dayCount int64 = ((pi / 3600.0) / 24.0)
 
 	if dayCount > 365*3 {
 		unixTime = time.Unix(31556926*3, 0)
-		dayCount = 365*3
+		dayCount = 365 * 3
 	}
-	
 
 	nowTime := time.Now()
 	prevTime := nowTime.Sub(unixTime)
 	nowTimeStr := fmt.Sprintf("%v", nowTime.Unix())
 	prevTimeStr := fmt.Sprintf("%v", int(prevTime.Seconds()))
-	timePeriod := MONTH_INTERVAL
-	if dayCount < 32 {
-		timePeriod = DAY_INTERVAL
-	} else if dayCount < 225 {
-		timePeriod = WEEK_INTEVAL
+	// timePeriod := MONTH_INTERVAL
+	// if dayCount < 32 {
+	// 	timePeriod = DAY_INTERVAL
+	// } else if dayCount < 225 {
+	// 	timePeriod = WEEK_INTEVAL
+	// }
+	//fmt.Println(timePeriod, dayCount)
+	//fmt.Println("Sending: ", prevTimeStr, nowTimeStr, timePeriod)
+	// Convert Unix timestamps to time.Time format
+	s1, e1 := strconv.Atoi(prevTimeStr)
+	s2, e2 := strconv.Atoi(nowTimeStr)
+	//limitArg, e3 :=
+	if e1 != nil || e2 != nil {
+		w.Write([]byte("Error occured with historical data retrival! " + e1.Error() + " or " + e2.Error()))
+		return
 	}
-	fmt.Println(timePeriod, dayCount)
+	time1 := time.Unix(int64(s1), 0).UTC()
+	time2 := time.Unix(int64(s2), 0).UTC()
 
-	csvData, err := getCSVData(quote, prevTimeStr, nowTimeStr, timePeriod, <- fakeUserAgents)
+	// Format the timestamps to a human-readable format
+	// fmt.Println("Timestamp 1:", time1.Format("2006-01-02"))
+
+	csvData, err := getCSVData(quote, time1.Format("2006-01-02"), time2.Format("2006-01-02"), strconv.Itoa(int(dayCount)), <-fakeUserAgents)
 	if err != nil {
 		w.Write([]byte("Error occured with historical data retrival! " + err.Error()))
 		return
 	}
-	w.Write([]byte(csvData))
+	var returnData struct {
+		Data struct {
+			TradesTable struct {
+				Rows []struct {
+					Date   string `json:"date"`
+					Close  string `json:"close"`
+					Volume string `json:"volume"`
+					Open   string `json:"open"`
+					High   string `json:"high"`
+					Low    string `json:"low"`
+				} `json:"rows"`
+			} `json:"tradesTable"`
+		} `json:"data"`
+	}
+
+	returnCSV := make([]struct {
+		UnixTime string `json:"time"`
+		Value    string `json:"value"`
+	}, 0)
+
+	err = json.Unmarshal([]byte(csvData), &returnData)
+	if err != nil {
+		w.Write([]byte("Error occured with historical data retrival! " + err.Error()))
+		return
+	}
+	// Process each row to convert date to Unix time and extract close value
+	for _, val := range returnData.Data.TradesTable.Rows {
+		// Parse the date to time.Time object
+		//date, err := time.Parse("01/02/2006", val.Date)
+		//if err != nil {
+		//	fmt.Printf("Error parsing date: %v\n", err)
+		//	continue
+		//}
+
+		//// Convert to Unix timestamp
+		//unixTime := date.Unix()
+
+		// Clean up the "close" value by removing the "$" sign
+		closeValue := strings.TrimPrefix(val.Close, "$")
+
+		// Append to the returnCSV slice
+		returnCSV = append(returnCSV, struct {
+			UnixTime string `json:"time"`
+			Value    string `json:"value"`
+		}{
+			//UnixTime: fmt.Sprintf("%d", unixTime),
+			UnixTime: val.Date,
+			Value:    closeValue,
+		})
+	}
+
+	// Convert returnCSV to CSV format and write to response
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=data.csv")
+	csvWriter := csv.NewWriter(w)
+	defer csvWriter.Flush()
+
+	// Write header
+	csvWriter.Write([]string{"time", "value"})
+
+	// Write the rows
+	for _, row := range returnCSV {
+		csvWriter.Write([]string{row.UnixTime, row.Value})
+	}
+
+	// Print debug info
+	//fmt.Println("Received CSV Data:")
+	for _, row := range returnCSV {
+		fmt.Printf("Time: %v, Value: %v\n", row.UnixTime, row.Value)
+	}
 }
 
 func allowOrigin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO: Only allow 2 URLs. Current origin and gtequityResearch.
-		next.ServeHTTP(w, r);
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -415,7 +537,7 @@ func main() {
 			}
 
 			// Check again after 10 seconds otherwise server go BURRRR
-			time.Sleep(10*time.Second)
+			time.Sleep(10 * time.Second)
 		}
 	}()
 	// GetNextCountPrices(5, "AAPL", c)
